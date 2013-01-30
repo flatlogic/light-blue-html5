189c013f2d2e11391b7dd8306943e35c17893161
oken <philip.daineka@gmail.com>
2013-01-30 08:53:36 +0300

    colors, headers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 var colors = function(){
-    return d3.scale.ordinal().range(['#1BA1E2', '#8cbf26', '#f0aa25', '#8cbf26', '#cd4e21', '#ea99ac']);
+    //return d3.scale.ordinal().range(['#1BA1E2', '#8cbf26', '#f0aa25', '#8cbf26', '#cd4e21', '#ea99ac']);
     //return d3.scale.category20c();
-    //return d3.scale.ordinal().range(["#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2"]);
+    return d3.scale.ordinal().range(["#e5603b", "#eac85e", "#56bc76", "#618fb0"]);
+    /*
+    * .box.red{
+     background: #e5603b;
+     }
+
+     .box.orange{
+     background: #eac85e;
+     }
+
+     .box.green{
+     background: #56bc76;
+     }
+
+     .box.blue{
+     background: #618fb0;
+     }
+    */
 }();
 
 function keyColor(d, i) {
