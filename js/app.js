9cc3bebce17b5331503f6ff389591f5648fbcd0a
oken <philip.daineka@gmail.com>
2013-01-30 13:13:17 +0300

    second widget done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var colors = function(){
     //return d3.scale.ordinal().range(['#1BA1E2', '#8cbf26', '#f0aa25', '#8cbf26', '#cd4e21', '#ea99ac']);
     //return d3.scale.category20c();
-    return d3.scale.ordinal().range(["#e5603b", "#eac85e", "#56bc76", "#618fb0"]);
+    return d3.scale.ordinal().range(["#e5603b", "#eac85e", "#56bc76", "#618fb0", '#2eb4dc', '#d04f4f']);
     /*
     * .box.red{
      background: #e5603b;
@@ -18,6 +18,14 @@ var colors = function(){
      .box.blue{
      background: #618fb0;
      }
+
+     .box.teal{
+     background: #2eb4dc;
+     }
+
+     .box.dark-red{
+     background: #d04f4f;
+     }
     */
 }();
 
@@ -25,13 +33,13 @@ function keyColor(d, i) {
     return colors(d.key)
 }
 
-function testData(stream_names) {
+function testData(stream_names, points_count) {
     var now = new Date().getTime(),
         day = 1000 * 60 * 60 * 24, //milliseconds
         days_ago_count = 90,
         days_ago = days_ago_count * day,
         days_ago_date = now - days_ago,
-        points_count = 45, //less for better performance
+        points_count = points_count || 45, //less for better performance
         day_per_point = days_ago_count / points_count;
     return stream_layers(stream_names.length, points_count, .08).map(function(data, i) {
         return {
--- js/index.js
+++ js/index.js
@@ -16,3 +16,103 @@ nv.addGraph(function() {
 
     return chart;
 });
+
+nv.addGraph(function() {
+    var chart = nv.models.stackedAreaChart()
+        .margin({left: 0, right: 0, top: 0, bottom: 0})
+        //todo this is the problem. stackedAreaChart shouldn't have some default margin. It's bad
+        .color(keyColor)
+        .showLegend(false)
+        .showControls(false)
+        .showAxes(false)
+        .tooltip(function(key, x, y) {
+            return '<p>' +  y + '</p>'
+        });
+
+    d3.select("#chart-visits svg")
+        .datum(testData(['Visits'], 8))
+        .transition().call(chart);
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.stackedAreaChart()
+        .margin({left: 0, right: 0, top: 0, bottom: 0})
+        //todo this is the problem. stackedAreaChart shouldn't have some default margin. It's bad
+        .color(keyColor)
+        .showLegend(false)
+        .showControls(false)
+        .showAxes(false)
+        .tooltip(function(key, x, y) {
+            return '<p>' +  y + '</p>'
+        });
+
+    d3.select("#chart-unique svg")
+        .datum(testData(['Unique'], 8))
+        .transition().call(chart);
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.stackedAreaChart()
+        .margin({left: 0, right: 0, top: 0, bottom: 0})
+        //todo this is the problem. stackedAreaChart shouldn't have some default margin. It's bad
+        .color(keyColor)
+        .showLegend(false)
+        .showControls(false)
+        .showAxes(false)
+        .tooltip(function(key, x, y) {
+            return '<p>' +  y + '</p>'
+        });
+
+    d3.select("#chart-pageviews svg")
+        .datum(testData(['Pageviews'], 8))
+        .transition().call(chart);
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.stackedAreaChart()
+        .margin({left: 0, right: 0, top: 0, bottom: 0})
+        //todo this is the problem. stackedAreaChart shouldn't have some default margin. It's bad
+        .color(keyColor)
+        .showLegend(false)
+        .showControls(false)
+        .showAxes(false)
+        .tooltip(function(key, x, y) {
+            return '<p>' +  y + '</p>'
+        });
+
+    d3.select("#chart-duration svg")
+        .datum(testData(['Duration'], 8))
+        .transition().call(chart);
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.stackedAreaChart()
+        .margin({left: 0, right: 0, top: 0, bottom: 0})
+        //todo this is the problem. stackedAreaChart shouldn't have some default margin. It's bad
+        .color(keyColor)
+        .showLegend(false)
+        .showControls(false)
+        .showAxes(false)
+        .tooltip(function(key, x, y) {
+            return '<p>' +  y + '</p>'
+        });
+
+    d3.select("#chart-bounce svg")
+        .datum(testData(['Bounce'], 8))
+        .transition().call(chart);
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
