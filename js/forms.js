e7fa5df0b4f84fb6f4edf4dc4eb5cb63f2fa2dda
Philip Daineka <daineko.f@gmail.com>
2013-03-02 12:53:39 +0300

    wisyhtml5 locale

--- js/forms.js
+++ js/forms.js
@@ -36,20 +36,23 @@ $(function(){
         "font-styles": function(locale) {
             return "<li class='dropdown'>" +
             "<a class='btn btn-small dropdown-toggle' data-toggle='dropdown' href='#'>" +
-            "<i class='icon-font'></i>&nbsp;<span class='current-font'>Normal text</span>&nbsp;&nbsp;<i class='icon-caret-down'></i>" +
+            "<i class='icon-font'></i>&nbsp;<span class='current-font'>" + locale.font_styles.normal + "</span>&nbsp;&nbsp;<i class='icon-caret-down'></i>" +
             "</a>" +
             "<ul class='dropdown-menu'>" +
-            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='div'>Normal text</a></li>" +
-            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h1'>Heading 1</a></li>" +
-            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h2'>Heading 2</a></li>" +
+            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='div'>" + locale.font_styles.normal + "</a></li>" +
+							"<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='p'>" + locale.font_styles.normal + "</a></li>" +
+							"<li><a data-wysihtml5-command='formatInline' data-wysihtml5-command-value='span'>" + locale.font_styles.normal + "</a></li>" +
+            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h1'>" + locale.font_styles.h1 + "</a></li>" +
+            "<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h2'>" + locale.font_styles.h2 + "</a></li>" +
+							"<li><a data-wysihtml5-command='formatBlock' data-wysihtml5-command-value='h3'>" + locale.font_styles.h3 + "</a></li>" +
             "</ul>" +
             "</li>"
         },
         "emphasis":  function(locale) {
             return "<li>" +
             "<div class='btn-group'>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='bold' title='CTRL+B'>Bold</a>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='italic' title='CTRL+I'>Italic</a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='bold' title='CTRL+B'><i class='icon-bold'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='italic' title='CTRL+I'><i class='icon-italic'></i></a>"
             //,+ "<a class='btn' data-wysihtml5-command='underline' title='CTRL+U'>Underline</a>"
             + "</div>"
             + "</li>"
@@ -57,10 +60,10 @@ $(function(){
         "lists": function(locale) {
                 return "<li>"
             + "<div class='btn-group'>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='insertUnorderedList' title='Unordered List'><i class='icon-list'></i></a>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='insertOrderedList' title='Ordered List'><i class='icon-th-list'></i></a>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='Outdent' title='Outdent'><i class='icon-indent-right'></i></a>"
-            + "<a class='btn btn-small ' data-wysihtml5-command='Indent' title='Indent'><i class='icon-indent-left'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='insertUnorderedList' title='" + locale.lists.unordered + "'><i class='icon-list'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='insertOrderedList' title='" + locale.lists.ordered + "'><i class='icon-th-list'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='Outdent' title='" + locale.lists.outdent + "'><i class='icon-indent-right'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='Indent' title='" + locale.lists.indent + "'><i class='icon-indent-left'></i></a>"
             + "</div>"
             + "</li>"
         },
@@ -71,18 +74,18 @@ $(function(){
             + "<div class='bootstrap-wysihtml5-insert-link-modal modal hide fade'>"
             + "<div class='modal-header'>"
             + "<a class='close' data-dismiss='modal'>×</a>"
-            + "<h3>Insert Link</h3>"
+            + "<h3>" + locale.link.insert + "</h3>"
             + "</div>"
             + "<div class='modal-body'>"
             + "<input value='http://' class='bootstrap-wysihtml5-insert-link-url input-xlarge'>"
             + "</div>"
             + "<div class='modal-footer'>"
-            + "<a href='#' class='btn' data-dismiss='modal'>Cancel</a>"
-            + "<a href='#' class='btn btn-primary' data-dismiss='modal'>Insert link</a>"
+            + "<a href='#' class='btn' data-dismiss='modal'>" + locale.link.cancel + "</a>"
+            + "<a href='#' class='btn btn-primary' data-dismiss='modal'>" + locale.link.insert + "</a>"
             + "</div>"
             + "</div>"
 
-            + "<a class='btn btn-small ' data-wysihtml5-command='createLink' title='Link'><i class='icon-share'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='createLink' title='" + locale.link.insert + "'><i class='icon-share'></i></a>"
 
             + "</li>"
         },
@@ -93,18 +96,18 @@ $(function(){
             + "<div class='bootstrap-wysihtml5-insert-image-modal modal hide fade'>"
             + "<div class='modal-header'>"
             + "<a class='close' data-dismiss='modal'>×</a>"
-            + "<h3>Insert Image</h3>"
+            + "<h3>" + locale.image.insert + "</h3>"
             + "</div>"
             + "<div class='modal-body'>"
             + "<input value='http://' class='bootstrap-wysihtml5-insert-image-url input-xlarge'>"
             + "</div>"
             + "<div class='modal-footer'>"
-            + "<a href='#' class='btn' data-dismiss='modal'>Cancel</a>"
-            + "<a href='#' class='btn btn-primary' data-dismiss='modal'>Insert image</a>"
+            + "<a href='#' class='btn' data-dismiss='modal'>" + locale.image.cancel + "</a>"
+            + "<a href='#' class='btn btn-primary' data-dismiss='modal'>" + locale.image.insert + "</a>"
             + "</div>"
             + "</div>"
 
-            + "<a class='btn btn-small ' data-wysihtml5-command='insertImage' title='Insert image'><i class='icon-picture'></i></a>"
+            + "<a class='btn btn-small ' data-wysihtml5-command='insertImage' title='" + locale.image.insert + "'><i class='icon-picture'></i></a>"
 
             + "</li>"
         },
@@ -112,7 +115,7 @@ $(function(){
         "html": function(locale) {
                             return "<li>"
                 + "<div class='btn-group'>"
-                + "<a class='btn btn-small ' data-wysihtml5-action='change_view' title='Edit HTML'><i class='icon-pencil'></i></a>"
+                + "<a class='btn btn-small ' data-wysihtml5-action='change_view' title='" + locale.html.edit + "'><i class='icon-pencil'></i></a>"
                 + "</div>"
                 + "</li>"
         }
