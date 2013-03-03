ada5b30e0ef4171844202dd82769127d0b46b094
philip.daineka@gmail.com <philip.daineka@gmail.com>
2013-03-03 12:24:29 +0200

    form elements inputs completed

--- js/forms-elemets.js
+++ js/forms-elemets.js
@@ -1,3 +1,8 @@
 $(function(){
     $("#tooltip-enabled, #max-length").tooltip();
+
+    $('.selectpicker').selectpicker();
+    //selectpicker doesn't seem to be flexible enough (can't change template), so need to replace span.caret externally
+    $('.selectpicker + .bootstrap-select span.caret').replaceWith("<i class='icon-caret-down'></i>");
+    $('.selectpicker + .bootstrap-select span.pull-left').removeClass("pull-left");
 });
