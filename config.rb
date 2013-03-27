5038dc74a3c09edf4fd2c1b4630ca5373b49deda
philip.daineka@gmail.com <philip.daineka@gmail.com>
2013-03-27 18:50:22 +0300

    compass variables fix

--- config.rb
+++ config.rb
@@ -12,6 +12,8 @@ javascripts_dir = "js"
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
+environment = :development
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
