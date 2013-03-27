cafee572060fadde06e4e583e3b5ff0f96cd8927
philip.daineka@gmail.com <philip.daineka@gmail.com>
2013-03-27 21:09:31 +0300

    application.min.css

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ javascripts_dir = "js"
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
-environment = :development
+environment = :development #:production
 output_style = (environment == :production) ? :compressed : :expanded
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
