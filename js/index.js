44365fc4f2ec58e68945b140df391ea2e07bddce
philip.daineka@gmail.com <philip.daineka@gmail.com>
2013-04-04 21:38:44 +0300

    i am gonna do it

--- js/index.js
+++ js/index.js
@@ -21,126 +21,6 @@ nv.addGraph(function() {
 });
 
 nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-        .color(keyColor)
-        .showLegend(false)
-        .showControls(false)
-        .showAxes(false)
-        .tooltip(function(key, x, y) {
-            return '<p>' +  y + '</p>'
-        });
-
-    d3.select("#chart-visits svg")
-        .datum(
-            //Test data. Just to see it's really simple
-            [{
-                "key" : "Visits",
-                "values" : [{
-                    "x" : 1,
-                    "y" : 0
-                },{
-                    "x" : 2,
-                    "y" : 2
-                },{
-                    "x" : 3,
-                    "y" : 3
-                },{
-                    "x" : 4,
-                    "y" : 2
-                },{
-                    "x" : 5,
-                    "y" : 1
-                },{
-                    "x" : 6,
-                    "y" : 4
-                },{
-                    "x" : 7,
-                    "y" : 5
-                },{
-                    "x" : 8,
-                    "y" : 6
-                }]
-            }]
-        )
-        .transition().call(chart);
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-        .color(keyColor)
-        .showLegend(false)
-        .showControls(false)
-        .showAxes(false)
-        .tooltip(function(key, x, y) {
-            return '<p>' +  y + '</p>'
-        });
-
-    d3.select("#chart-unique svg")
-        .datum(testData(['Unique'], 8))
-        .transition().call(chart);
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-        .color(keyColor)
-        .showLegend(false)
-        .showControls(false)
-        .showAxes(false)
-        .tooltip(function(key, x, y) {
-            return '<p>' +  y + '</p>'
-        });
-
-    d3.select("#chart-pageviews svg")
-        .datum(testData(['Pageviews'], 8))
-        .transition().call(chart);
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-        .color(keyColor)
-        .showLegend(false)
-        .showControls(false)
-        .showAxes(false)
-        .tooltip(function(key, x, y) {
-            return '<p>' +  y + '</p>'
-        });
-
-    d3.select("#chart-duration svg")
-        .datum(testData(['Duration'], 8))
-        .transition().call(chart);
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-nv.addGraph(function() {
-    var chart = nv.models.stackedAreaChart()
-        .color(keyColor)
-        .showLegend(false)
-        .showControls(false)
-        .showAxes(false)
-        .tooltip(function(key, x, y) {
-            return '<p>' +  y + '</p>'
-        });
-
-    d3.select("#chart-bounce svg")
-        .datum(testData(['Bounce'], 8))
-        .transition().call(chart);
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-nv.addGraph(function() {
     var testData = [
         {
             key: "Audio",
@@ -219,5 +99,44 @@ nv.addGraph(function() {
 });
 
 $(function(){
+
+    /* Sparklines can also take their values from the first argument
+     passed to the sparkline() function */
+    function randomValue(){
+        return Math.floor( Math.random() * 40 );
+    }
+    var values = [[],[],[],[],[],[]],
+        options = {
+            width: '100%',
+            height: '100%',
+            lineColor: '#777',
+            fillColor: 'white',
+            lineWidth: '2',
+            spotRadius: '4',
+            maxSpotColor: $orange,
+            minSpotColor: $green,
+            spotColor: $red,
+            highlightLineColor: '#777',
+            highlightSpotColor: $blue
+        };
+    for (var i = 0; i < values.length; i++){
+        values[i] = [10 + randomValue(), 15 + randomValue(), 20 + randomValue(), 15 + randomValue(), 25 + randomValue(),
+            25 + randomValue(), 30 + randomValue(), 30 + randomValue(), 40 + randomValue()]
+    }
+
+    function drawSparkLines(){
+        var i = 0;
+        $('.sparkline').each(function(){
+            $(this).sparkline(values[i], options );
+            i++;
+        });
+    }
+    var sparkResize;
+
+    $(window).resize(function(e) {
+        clearTimeout(sparkResize);
+        sparkResize = setTimeout(drawSparkLines, 200);
+    });
+    drawSparkLines();
     $("input:checkbox").uniform();
 });
