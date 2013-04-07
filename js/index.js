8418c78bb0ec13d90d53c2cf26e79326ecc072c3
philip.daineka@gmail.com <philip.daineka@gmail.com>
2013-04-07 18:55:41 +0300

    logo visibility, notifications link

--- js/index.js
+++ js/index.js
@@ -139,4 +139,23 @@ $(function(){
     });
     drawSparkLines();
     $("input:checkbox").uniform();
+
+    // Notification link click handler.
+    // JUST FOR DEMO.
+    // Can be removed.
+
+    function close(e){
+        var $settings = $("#settings"),
+            $popover = $settings.siblings(".popover");
+        if(!$.contains($popover[0], e.target)){
+            $settings.popover('hide');
+            $(document).off("click", close);
+        }
+    }
+    $("#notification-link").click(function(){
+        $("#settings").popover('show');
+        $(document).on("click", close);
+        return false;
+    })
 });
+
