650e4883d1dd8db8534c0bbf6b8776b33b3562bb
okendoken <philip.daineka@gmail.com>
2013-06-28 15:17:48 +0300

    form-wizard responsive, finish button

--- js/wizard.js
+++ js/wizard.js
@@ -1,9 +1,22 @@
 $(function(){
     $('.chzn-select').select2();
-    $("#rootwizard").bootstrapWizard({onTabShow: function(tab, navigation, index) {
+    $("#destination").mask("99999");
+    $("#credit").mask("9999-9999-9999-9999");
+    $("#expiration-date").datepicker();
+    $("#wizard").bootstrapWizard({onTabShow: function(tab, navigation, index) {
         var $total = navigation.find('li').length;
         var $current = index+1;
         var $percent = ($current/$total) * 100;
-        $('#rootwizard').find('.bar').css({width:$percent+'%'});
+        var $wizard = $("#wizard");
+        $wizard.find('.bar').css({width:$percent+'%'});
+
+        if($current >= $total) {
+            $wizard.find('.pager .next').hide();
+            $wizard.find('.pager .finish').show();
+            $wizard.find('.pager .finish').removeClass('disabled');
+        } else {
+            $wizard.find('.pager .next').show();
+            $wizard.find('.pager .finish').hide();
+        }
     }});
 });
