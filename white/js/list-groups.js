859418778d66b386068c4b516d0ec7ccbb38af09
okendoken <philip.daineka@gmail.com>
2013-11-07 18:38:49 +0300

    nestable

--- white/js/list-groups.js
+++ white/js/list-groups.js
@@ -3,4 +3,10 @@ $(function(){
         placeholder: 'list-group-item list-group-item-placeholder',
         forcePlaceholderSize: true
     });
+    $("#nestable1").nestable({
+        group: 1
+    });
+    $("#nestable2").nestable({
+        group: 1
+    });
 });
