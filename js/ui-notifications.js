51c1eb4e59ec5dfbff59942679b521aa74180e3e
okendoken <philip.daineka@gmail.com>
2014-04-28 23:21:49 +0300

    notifications page

--- js/ui-notifications.js
+++ js/ui-notifications.js
@@ -36,31 +36,90 @@
 $(function(){
     function pageLoad(){
         $('.widget').widgster();
-        Messenger({
-            theme: 'air'
-        }).post({
-            message: "Thanks for checking out Messenger!",
-            type: 'error',
-            showCloseButton: true,
-            hideAfter: false
+
+        var theme = 'air';
+
+        $.globalMessenger({ theme: theme });
+        Messenger.options = { theme: theme  };
+
+        Messenger().post("Thanks for checking out Messenger!");
+
+
+        var loc = ['bottom', 'right'];
+
+        var $lsel = $('.location-selector');
+
+        var update = function(){
+            var classes = 'messenger-fixed';
+
+            for (var i=0; i < loc.length; i++)
+                classes += ' messenger-on-' + loc[i];
+
+            $.globalMessenger({ extraClasses: classes, theme: theme  });
+            Messenger.options = { extraClasses: classes, theme: theme };
+        };
+
+        update();
+
+        $lsel.locationSelector()
+            .on('update', function(pos){
+                loc = pos;
+
+                update();
+            });
+
+        $('#show-error-message').on('click', function(){
+            var i;
+
+            i = 0;
+
+            Messenger().run({
+                errorMessage: 'Error destroying alien planet',
+                successMessage: 'Alien planet destroyed!',
+                action: function(opts) {
+                    if (++i < 3) {
+                        return opts.error({
+                            status: 500,
+                            readyState: 0,
+                            responseText: 0
+                        });
+                    } else {
+                        return opts.success();
+                    }
+                }
+            });
+
+            return false;
         });
 
-        var i = 0;
-
-        Messenger().run({
-            errorMessage: 'Error destroying alien planet',
-            successMessage: 'Alien planet destroyed!',
-            action: function(opts) {
-                if (++i < 3) {
-                    return opts.error({
-                        status: 500,
-                        readyState: 0,
-                        responseText: 0
-                    });
-                } else {
-                    return opts.success();
+        $('#show-info-message').on('click', function(){
+            var msg = Messenger().post({
+                message: 'Launching thermonuclear war...',
+                actions: {
+                    cancel: {
+                        label: 'cancel launch',
+                        action: function() {
+                            return msg.update({
+                                message: 'Thermonuclear war averted',
+                                type: 'success',
+                                actions: false
+                            });
+                        }
+                    }
                 }
-            }
+            });
+
+            return false;
+        });
+
+        $('#show-success-message').on('click', function(){
+            Messenger().post({
+                message: 'Showing success message was successful!',
+                type: 'success',
+                showCloseButton: true
+            });
+
+            return false;
         });
     }
 
